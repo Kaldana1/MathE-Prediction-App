30e0e3304c04553f57514a418bd9ffac9dfc4cae
Kaldana1 <justinkaldana@gmail.com>
2025-03-05 14:43:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 # import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import joblib  # Pour charger ton modèle
 import numpy as np
 # Message de bienvenu
